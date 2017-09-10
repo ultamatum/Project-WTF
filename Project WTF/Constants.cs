a6940ba145cfa98907d24173d162f64e7b4e3815
Ultamatum <alexanderg0502@gmail.com>
2017-09-10 21:50:45 +0100

    Add project files.

---
+++ Project WTF/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_WTF
+{
+    public static class Constants
+    {
+        public const int SCREENWIDTH = 1920;
+        public const int SCREENHEIGHT = 1080;
+
+        public const bool FULLSCREEN = false;
+    }
+}
---
+++ Project WTF/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Project_WTF
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Project_WTF())
+                game.Run();
+        }
+    }
+#endif
+}
