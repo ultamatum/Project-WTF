6820b7cc7c456b0d3f73dac5d06f2975e44dd867
Ultamatum <alexanderg0502@gmail.com>
2017-09-11 12:54:45 +0100

    Completed camera and mapsquare class

--- Project WTF/TileEngine/MapSquare.cs
+++ Project WTF/TileEngine/MapSquare.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace Project_WTF.TileEngine
 {
-    class MapSquare
+    [Serializable]
+    public class MapSquare
     {
+        #region Declarations
+        public int[] layerTiles = new int[3];           //The amount of layers on each tile
+        public string codeValue = "";                   //A value to dictate special traits of each tile
+        public bool passable = true;                    //Dictates whether or not the tile can be passed through
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Create a tile on the map with specific values
+        /// </summary>
+        /// <param name="background">The textureID of the background layer of the tile</param>
+        /// <param name="interactive">The textureID of the interactive layer of the tile</param>
+        /// <param name="foreground">The textureID of the foreground layer of the tile</param>
+        /// <param name="code">The codevalue of the tile</param>
+        /// <param name="passable">Is the tile passable</param>
+        public MapSquare(int background, int interactive, int foreground, string code, bool passable)
+        {
+            layerTiles[0] = background;
+            layerTiles[1] = interactive;
+            layerTiles[2] = foreground;
+            codeValue = code;
+            this.passable = passable;
+        }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Toggles the passable value of a block
+        /// </summary>
+        public void TogglePassable()
+        {
+            passable = !passable;
+        }
+        #endregion
     }
 }
